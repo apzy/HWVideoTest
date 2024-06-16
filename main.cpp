e5f96d305772d74362c8897cd5d716db81c363ce
zhouyang <zyzhouyang1@outlook.com>
2024-06-16 19:24:36 +0800

    实现视频转h264

--- main.cpp
+++ main.cpp
@@ -15,10 +15,20 @@
 #include <chrono>
 #include "NvCodec/NvEncoder/NvEncoderCuda.h"
 #include "Utils/NvEncoderCLIOptions.h"
+#include "Utils/FFmpegStreamer.h"
 
 using NvEncCudaPtr = std::unique_ptr<NvEncoderCuda, std::function<void(NvEncoderCuda*)>>;
 
 simplelogger::Logger* logger = simplelogger::LoggerFactory::CreateConsoleLogger();
+
+auto EncodeDeleteFunc = [](NvEncoderCuda* pEnc)
+	{
+		if (pEnc)
+		{
+			pEnc->DestroyEncoder();
+			delete pEnc;
+		}
+	};
 
 void GetImage(CUdeviceptr dpSrc, uint8_t* pDst, int nWidth, int nHeight)
 {
@@ -47,7 +57,21 @@ int FindMin(volatile int* a, int n)
 	return r;
 }
 
-void TranscodeOneToN(NvDecoder* pDec, FFmpegDemuxer* pDemuxer, const char* szOutFileNamePrefix)
+template<class EncoderClass>
+void InitializeEncoder(EncoderClass& pEnc, NvEncoderInitParam encodeCLIOptions, NV_ENC_BUFFER_FORMAT eFormat)
+{
+	NV_ENC_INITIALIZE_PARAMS initializeParams = { NV_ENC_INITIALIZE_PARAMS_VER };
+	NV_ENC_CONFIG encodeConfig = { NV_ENC_CONFIG_VER };
+
+	initializeParams.encodeConfig = &encodeConfig;
+	pEnc->CreateDefaultEncoderParams(&initializeParams, encodeCLIOptions.GetEncodeGUID(), encodeCLIOptions.GetPresetGUID(), encodeCLIOptions.GetTuningInfo());
+	encodeCLIOptions.SetInitParams(&initializeParams, eFormat);
+
+	pEnc->CreateEncoder(&initializeParams);
+}
+
+
+void TranscodeOneToN(CUcontext cuContext, NV_ENC_BUFFER_FORMAT eFormat, NvEncoderInitParam encodeCLIOptions, std::ofstream& fpOut, NvDecoder* pDec, FFmpegDemuxer* pDemuxer, const char* szOutFileNamePrefix)
 {
 	const int nSrcFrame = 8;
 
@@ -60,7 +84,6 @@ void TranscodeOneToN(NvDecoder* pDec, FFmpegDemuxer* pDemuxer, const char* szOut
 	int nVideoBytes = 0, nFrameReturned = 0, nFrame = 0;
 	uint8_t* pVideo = NULL, * pFrame = NULL;
 
-	// aiEnc[i] holds the next frame to be encoded by encoder instance i.
 	int nWidth, nHeight;
 	CUdeviceptr pTmpImage = 0;
 	cuMemAlloc(&pTmpImage, 1920 * 1080 * 4);
@@ -72,6 +95,11 @@ void TranscodeOneToN(NvDecoder* pDec, FFmpegDemuxer* pDemuxer, const char* szOut
 
 	cuMemAllocPitch((CUdeviceptr*)&dstFrame, &dstPitch, dstWidth, dstHeight + (dstHeight / 2), 16);
 	cv::Mat show(cv::Size(dstWidth, dstHeight), CV_8UC4);
+
+
+	std::unique_ptr<NvEncoderCuda> pEnc(new NvEncoderCuda(cuContext, dstWidth, dstHeight, eFormat));
+	InitializeEncoder(pEnc, encodeCLIOptions, eFormat);
+
 	do
 	{
 		pDemuxer->Demux(&pVideo, &nVideoBytes);
@@ -79,43 +107,50 @@ void TranscodeOneToN(NvDecoder* pDec, FFmpegDemuxer* pDemuxer, const char* szOut
 
 		for (int i = 0; i < nFrameReturned; i++)
 		{
+			std::vector<std::vector<uint8_t>> vPacket;
 			pFrame = pDec->GetFrame();
 			int iMatrix = pDec->GetVideoFormatInfo().video_signal_description.matrix_coefficients;
-			printf("%d %d %d\n", pDec->GetDeviceFramePitch(), pDemuxer->GetWidth(), pDemuxer->GetHeight());
 
 			ResizeNv12(dstFrame, dstPitch, dstWidth, dstHeight, pFrame, pDec->GetDeviceFramePitch(), pDemuxer->GetWidth(), pDemuxer->GetHeight());
 			Nv12ToColor32<BGRA32>(dstFrame, dstPitch, (uint8_t*)pTmpImage, 4 * dstWidth,
 				dstWidth, dstHeight, iMatrix);
-			GetImage(pTmpImage, show.data, 4 * dstWidth, dstHeight);
-			cv::imshow("frame", show);
-			cv::waitKey(0);
+
+			const NvEncInputFrame* encoderInputFrame = pEnc->GetNextInputFrame();
+			cuMemcpy((CUdeviceptr)encoderInputFrame->inputPtr, (CUdeviceptr)pTmpImage, dstWidth * dstHeight * 4);
+
+			pEnc->EncodeFrame(vPacket);
+
+			//GetImage(pTmpImage, show.data, 4 * dstWidth, dstHeight);
+			//cv::imshow("frame", show);
+			//cv::waitKey(1);
+			for (std::vector<uint8_t>& packet : vPacket)
+			{
+				fpOut.write(reinterpret_cast<char*>(packet.data()), packet.size());
+			}
 		}
 
 	}
 	while (nVideoBytes);
-
-	bEnd = true;
-	for (auto& pth : vpth)
+	std::vector<std::vector<uint8_t>> vPacket;
+	pEnc->EndEncode(vPacket);
+	for (std::vector<uint8_t>& packet : vPacket)
 	{
-		pth.join();
+		fpOut.write(reinterpret_cast<char*>(packet.data()), packet.size());
 	}
-	for (int i = 0; i < nSrcFrame; i++)
-	{
-		if (apSrcFrame[i])
-		{
-			pDec->UnlockFrame(&apSrcFrame[i]);
-		}
-	}
-
+	pEnc->DestroyEncoder();
 }
+
 
 int main(int argc, char* argv[])
 {
+
+	NvEncoderInitParam encodeCLIOptions;
+
 	int iGpu = 0;
 	char szInFilePath[260] = "input.mp4";
 	char szOutFileNamePrefix[260] = "out";
 	std::vector<int2> vResolution;
-	vResolution.push_back(make_int2(600, 480));
+	vResolution.push_back(make_int2(640, 480));
 	std::vector<std::exception_ptr> vExceptionPtrs;
 	try
 	{
@@ -129,7 +164,17 @@ int main(int argc, char* argv[])
 
 		FFmpegDemuxer demuxer(szInFilePath);
 		NvDecoder dec(cuContext, true, FFmpeg2NvCodecId(demuxer.GetVideoCodec()), false, true);
-		TranscodeOneToN(&dec, &demuxer, szOutFileNamePrefix);
+
+		std::ofstream fpOut("output.h264", std::ios::out | std::ios::binary);
+		if (!fpOut)
+		{
+			std::ostringstream err;
+			throw std::invalid_argument(err.str());
+		}
+
+
+		TranscodeOneToN(cuContext, NV_ENC_BUFFER_FORMAT_ARGB, encodeCLIOptions, fpOut, &dec, &demuxer, szOutFileNamePrefix);
+		fpOut.close();
 
 	}
 	catch (const std::exception& ex)
